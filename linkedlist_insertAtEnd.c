438ac37cb6b768e6e1e8af0d273fab96bde9e7c4
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-08-17 12:23:19 +0530

    insertion of number at end in a linked list

---
+++ linkedlist_insertAtEnd.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void traversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *Insert_At_End(struct Node *head, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    ptr->data = data;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    ptr->next = NULL;
+    p->next = ptr;
+
+    return head;
+}
+
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *first = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = first;
+
+    first->data = 11;
+    first->next = second;
+
+    second->data = 45;
+    second->next = third;
+
+    third->data = 67;
+    third->next = NULL;
+
+    printf("before insertion:\n");
+    traversal(head);
+
+    printf("\nafter insertion:\n");
+    head = Insert_At_End(head, 2);
+    traversal(head);
+
+    return 0;
+}
