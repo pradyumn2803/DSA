26e518986b606526600502bca1e9fdf9eea426e8
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-08-17 13:13:14 +0530

    deletes the last element in linked list

---
+++ linkedlist_deleteAtEnd.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void traverse(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *Delete_At_End(struct Node *head)
+{
+    struct Node *p = head;
+    struct Node *q = p->next;
+
+    while (q->next != NULL)
+    {
+        p = p->next;
+        q = q->next;
+    }
+
+    p->next = NULL;
+    free(q);
+    return head;
+}
+
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *first = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = first;
+
+    first->data = 12;
+    first->next = second;
+
+    second->data = 21;
+    second->next = third;
+
+    third->data = 32;
+    third->next = NULL;
+
+    printf("Before deletion:\n");
+    traverse(head);
+
+    printf("After deletion:\n");
+    head = Delete_At_End(head);
+    traverse(head);
+
+    return 0;
+}
