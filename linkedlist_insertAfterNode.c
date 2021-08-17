1ad6c35c0d02ae59fd9a3a9b491de7403f5911ce
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-08-17 12:23:46 +0530

    insertion of number after a given node in a linked list

---
+++ linkedlist_insertAfterNode.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void traversal(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *Insert_After_node(struct Node *head, struct Node *prevnode, int data)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    ptr->data=data;
+    ptr->next = prevnode->next;
+    prevnode->next = ptr;
+
+    return head;
+}
+
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *first = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 7;
+    head->next = first;
+
+    first->data = 11;
+    first->next = second;
+
+    second->data = 45;
+    second->next = third;
+
+    third->data = 67;
+    third->next = NULL;
+
+    printf("before insertion:\n");
+    traversal(head);
+
+    printf("\nafter insertion:\n");
+    head = Insert_After_node(head, first, 3);
+    traversal(head);
+
+    return 0;
+}
