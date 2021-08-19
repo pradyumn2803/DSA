5a4d491ec04dbdbf5adbc088bc7db0f3c50c8e6c
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-08-19 14:20:58 +0530

    added infix to postfix

--- infix_to_postfix.c
+++ infix_to_postfix.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 struct stack
 {
@@ -9,112 +9,127 @@ struct stack
     char *arr;
 };
 
-int precedence(char ch){
-    if (ch=='*' || ch=='/')
+int precedence(char ch)
+{
+    if (ch == '*' || ch == '/')
     {
         return 3;
     }
 
-    if (ch=='+' || ch=='-')
+    if (ch == '+' || ch == '-')
     {
         return 2;
     }
-    return 0;   
+    return 0;
 }
 
-int is_operator(char ch){
-    if (ch=='*' || ch=='/' || ch=='+' || ch=='-')
+int is_operator(char ch)
+{
+    if (ch == '*' || ch == '/' || ch == '+' || ch == '-')
     {
         return 1;
     }
-    return 0;   
+    return 0;
 }
 
-int is_empty(struct stack* sp){
-    if(sp->top==-1){
+int is_empty(struct stack *sp)
+{
+    if (sp->top == -1)
+    {
         return 1;
     }
     return 0;
 }
 
-int is_full(struct stack*sp){
-    if(sp->top==sp->size-1){
+int is_full(struct stack *sp)
+{
+    if (sp->top == sp->size - 1)
+    {
         return 1;
     }
     return 0;
 }
 
-char push(struct stack* sp,char ch){
-    if(is_full(sp)){
+char push(struct stack *sp, char ch)
+{
+    if (is_full(sp))
+    {
         printf("Stack overflow!!");
     }
-    else{
+    else
+    {
         sp->top++;
-        sp->arr[sp->top]=ch;
+        sp->arr[sp->top] = ch;
     }
 }
 
-char pop(struct stack* sp){
+char pop(struct stack *sp)
+{
     char ch;
-    if(is_empty(sp)){
+    if (is_empty(sp))
+    {
         printf("Stack underflow!!");
     }
     else
     {
-        ch=sp->arr[sp->top];
+        ch = sp->arr[sp->top];
         sp->top--;
     }
     return ch;
-    
 }
 
-char stack_Top(struct stack* sp){
+char stack_Top(struct stack *sp)
+{
     return sp->arr[sp->top];
 }
 
-char *infix_to_postfix(char *ch){
-    struct stack* sp = (struct stack *)malloc(sizeof(struct stack));
-    sp->size=strlen(ch)+1;
-    sp->top=-1;
-    sp->arr=(char*)malloc(sp->size*sizeof(char)); // for push and pop
-    char* postfix=(char*)malloc(sp->size*sizeof(char)); 
+char *infix_to_postfix(char *ch)
+{
+    struct stack *sp = (struct stack *)malloc(sizeof(struct stack));
+    sp->size = strlen(ch) + 1;
+    sp->top = -1;
+    sp->arr = (char *)malloc(sp->size * sizeof(char)); // for push and pop
+    char *postfix = (char *)malloc(sp->size * sizeof(char));
 
-    int i=0;// variable to traverse for infix array
-    int j=0; // variable to traverse for postfix array
+    int i = 0; // variable to traverse for infix array
+    int j = 0; // variable to traverse for postfix array
 
-    while(ch[i]!='\0'){
-        if(!(is_operator(ch[i]))){
-            postfix[j]=ch[i];
+    while (ch[i] != '\0')
+    {
+        if (!(is_operator(ch[i])))
+        {
+            postfix[j] = ch[i];
             i++;
             j++;
         }
-        else{
-            if(precedence(ch[i])>precedence(stack_Top(sp))){
-                push(sp,ch[i]);
+        else
+        {
+            if (precedence(ch[i]) > precedence(stack_Top(sp)))
+            {
+                push(sp, ch[i]);
                 i++;
             }
             else
             {
-                postfix[j]=pop(sp);
+                postfix[j] = pop(sp);
                 j++;
             }
-            
         }
     }
     while (!(is_empty(sp)))
     {
-        postfix[j]=pop(sp);
+        postfix[j] = pop(sp);
         j++;
     }
-    postfix[j]='\0';
+    postfix[j] = '\0';
 
     return postfix;
 }
 
 int main()
 {
-    char *c= "3*4+6/7",*p;
-    p=infix_to_postfix(c);
-    printf("%s",p);
+    char *c = "3*4+6/7", *p;
+    p = infix_to_postfix(c);
+    printf("%s", p);
     return 0;
 }
