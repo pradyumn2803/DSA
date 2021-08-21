7b0c43c432eb731b58eda5c9ed1160f665f8be47
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-08-21 11:13:21 +0530

    added queue basic operations

--- queue_using_arrays.c
+++ queue_using_arrays.c
@@ -9,7 +9,8 @@ struct queue
     int *arr;
 };
 
-void traverse(struct queue* q){
+void traverse(struct queue *q)
+{
     printf("The array is:\n");
     // while(q->f<q->r){
     //     printf("%d\n",q->arr[(q->f)+1]);
@@ -19,22 +20,23 @@ void traverse(struct queue* q){
 
     for (int i = q->f; i < q->r; i++)
     {
-        printf("%d\n",q->arr[(i)+1]);
+        printf("%d\n", q->arr[(i) + 1]);
     }
-
 }
 
-int first_value(struct queue* q){
-    return q->arr[q->f+1];
+int first_value(struct queue *q)
+{
+    return q->arr[q->f + 1];
 }
 
-int last_value(struct queue* q){
+int last_value(struct queue *q)
+{
     return q->arr[q->r];
 }
 
-int peek(struct queue* q,int pos){
-    return q->arr[q->f+pos];
-
+int peek(struct queue *q, int pos)
+{
+    return q->arr[q->f + pos];
 }
 
 int is_empty(struct queue *q)
@@ -64,79 +66,77 @@ void enqueue(struct queue *q, int val)
     else
     {
         q->r++;
-        q->arr[q->r]=val;
+        q->arr[q->r] = val;
     }
 }
 
-    int dequeue(struct queue * q)
+int dequeue(struct queue *q)
+{
+    if (is_empty(q))
     {
-        if (is_empty(q))
-        {
-            printf("stack underflow");
-        }
-        else
-        {
-            q->f++;
-            int val = q->arr[q->f];
-            return val;
-        }
+        printf("stack underflow");
+    }
+    else
+    {
+        q->f++;
+        int val = q->arr[q->f];
+        return val;
+    }
+}
+
+int main()
+{
+    struct queue *q = (struct queue *)malloc(sizeof(struct queue)); // NOTE: we are using a
+                                                                    //structure pointer so we are passing
+                                                                    //the pointer directly in to the function
+                                                                    //if we are using an object then we
+                                                                    //need to pass the address of the object
+    q->size = 10;
+    q->r = q->f = -1;
+    q->arr = (int *)malloc(q->size * sizeof(int));
+    int index;
+
+    enqueue(q, 45);
+    enqueue(q, 67);
+    enqueue(q, 4);
+    enqueue(q, 5);
+    enqueue(q, 22);
+    enqueue(q, 34);
+    traverse(q);
+
+    printf("First value:%d\n", first_value(q));
+    printf("Last Value:%d\n", last_value(q));
+    // printf("Enter the position(>0):");
+    // scanf("%d",&index);
+    // while(index<=0 || index >q->r){//since we are enqueueing 6 values
+    //     printf("Enter the position(>0):");
+    //     scanf("%d",&index);
+    // }
+    // printf("position %d:%d\n",index,peek(q,index));
+    for (int i = 0; i < (q->r) + 1; i++)
+    {
+        printf("position %d:%d\n", i + 1, peek(q, i + 1));
     }
 
-    int main()
+    printf("The value dequeue is %d\n", dequeue(q));
+    printf("The value dequeue is %d\n", dequeue(q));
+    printf("The value dequeue is %d\n", dequeue(q));
+    traverse(q);
+
+    // printf("Enter the position(>0):");
+    // scanf("%d",&index);
+    // while(index<=0 || index>(q->r-q->f)){// since we are dequeueing 3 values
+    //     printf("Enter the position(>0):");
+    //     scanf("%d",&index);
+    // }
+    // printf("position %d:%d\n\n",index,peek(q,index));
+
+    for (int i = 0; i < (q->r - q->f); i++)
     {
-        struct queue *q= (struct queue*)malloc(sizeof(struct queue));// NOTE: we are using a 
-                                                                        //structure pointer so we are passing 
-                                                                        //the pointer directly in to the function
-                                                                        //if we are using an object then we 
-                                                                        //need to pass the address 
-        q->size = 10;
-        q->r = q->f = -1;
-        q->arr = (int *)malloc(q->size * sizeof(int));
-        int index;
-
-        enqueue(q,45);
-        enqueue(q,67);
-        enqueue(q,4);
-        enqueue(q,5);
-        enqueue(q,22);
-        enqueue(q,34);
-        traverse(q);
-
-        printf("First value:%d\n",first_value(q));
-        printf("Last Value:%d\n",last_value(q));
-        // printf("Enter the position(>0):");
-        // scanf("%d",&index);
-        // while(index<=0 || index >q->r){// the index passed should be valid
-        //     printf("Enter the position(>0):");
-        //     scanf("%d",&index);
-        // } 
-        // printf("position %d:%d\n",index,peek(q,index));
-        for (int i = 0; i < q->r+1; i++)
-        {
-            printf("position %d:%d\n",i+1,peek(q,i+1));
-        }
-        
-
-
-        printf("The value dequeue is %d\n",dequeue(q));
-        printf("The value dequeue is %d\n",dequeue(q));
-        printf("The value dequeue is %d\n",dequeue(q));
-        traverse(q);
-        
-        // printf("Enter the position(>0):");
-        // scanf("%d",&index);
-        // while(index<=0 || index>(q->r-q->f)){//the index passed should be valid
-        //     printf("Enter the position(>0):");
-        //     scanf("%d",&index);
-        // }
-        // printf("position %d:%d\n\n",index,peek(q,index));
-
-        for (int i = 0; i < (q->r-q->f); i++)
-        {
-            printf("position %d:%d\n",i+1,peek(q,i+1));
-        }
-        printf("First value:%d\n",first_value(q));
-        printf("Last Value:%d\n",last_value(q));
-
-        return 0;
+        printf("position %d:%d\n", i + 1, peek(q, i + 1));
     }
+    printf("First value:%d\n", first_value(q));
+    printf("Last Value:%d\n", last_value(q));
+
+    return 0;
+}
