0c3057a9c9984a02ffdc0787a0348bc8e18bb460
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-10-22 12:17:07 +0530

    added vertical order

--- Trees_cpp/vertical_order.cpp
+++ Trees_cpp/vertical_order.cpp
@@ -1,76 +1,89 @@
+#include <bits/stdc++.h>
+using namespace std;
 /*
 time complexity-O(N)*logN
 space complexity-O(N)
 */
-#include <bits/stdc++.h>
-using namespace std;
-
 struct node
 {
     int data;
     struct node *left;
     struct node *right;
-
     node()
     {
         data = 0;
         left = right = NULL;
     }
-
-    node(int d)
+    node(int val)
     {
-        data = d;
+        data = val;
         left = right = NULL;
     }
-
-    node(int d, node *l, node *r)
+    node(int val, node *left, node *right)
     {
-        data = d;
-        left = l;
-        right = r;
+        data = val;
+        this->left = left;
+        this->right = right;
     }
 };
 
-class solution
+vector<vector<int>> vertical_t(node *root)
 {
-public:
-    vector<vector<int>> verticalTraversal(struct node *n)
+    vector<vector<int>> ans;
+    map<int, map<int, multiset<int>>> nodes;
+    queue<pair<node *, pair<int, int>>> q;
+    if (root == NULL)
     {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<node *, pair<int, int>>> todo;
-        todo.push({n, {0, 0}});
-        while (!todo.empty())
-        {
-            auto p = todo.front();
-            todo.pop();
-            node *node = p.first;
-            int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(node->data);
-            if (node->left)
-            {
-                todo.push({node->left, {x - 1, y + 1}});
-            }
-            if (node->right)
-            {
-                todo.push({node->right, {x + 1, y + 1}});
-            }
-        }
-        vector<vector<int>> ans;
-        for (auto p : nodes)
-        {
-            vector<int> col;
-            for (auto q : p.second)
-            {
-                col.insert(col.end(), q.second.begin(), q.second.end());
-            }
-            ans.push_back(col);
-        }
         return ans;
     }
-};
+    q.push({root, {0, 0}});
+    while (!q.empty())
+    {
+        node *nd = q.front().first;
+        int x = q.front().second.first;
+        int y = q.front().second.second;
+        nodes[x][y].insert(nd->data);
+        q.pop();
+        if (nd->left)
+        {
+            q.push({nd->left, {x - 1, y + 1}});
+        }
+        if (nd->right)
+        {
+            q.push({nd->right, {x + 1, y + 1}});
+        }
+    }
 
+    for (auto p : nodes)
+    {
+        vector<int> col;
+        for (auto q : p.second)
+        {
+            col.insert(col.end(), q.second.begin(), q.second.end());
+        }
+        ans.push_back(col);
+    }
+    return ans;
+}
 int main()
 {
-
+    node *root = new node(1);
+    root->left = new node(2);
+    root->right = new node(3);
+    root->left->left = new node(4);
+    root->right->right = new node(10);
+    root->left->right = new node(10);
+    root->right->left = new node(9);
+    root->left->left->right = new node(5);
+    root->left->left->right->right = new node(6);
+    vector<vector<int>> ans = vertical_t(root);
+    for (auto i : ans)
+    {
+        for (auto j : i)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
     return 0;
 }
