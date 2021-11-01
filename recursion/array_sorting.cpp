adad2d0f3d6b3e75f831af88b1a57ec194583295
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2021-11-01 13:27:38 +0530

    added sort

---
+++ recursion/array_sorting.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define ll long long int
+#define tc      \
+    ll t;       \
+    cin >> t;   \
+    while (t--) \
+        ;
+int arr[100005];
+
+void insert(vector<int> &v, int val)
+{
+    if (v.size() == 0 || v[v.size() - 1] <= val)
+    {
+        v.push_back(val);
+        return;
+    }
+    int temp = v[v.size() - 1];
+    v.pop_back();
+    insert(v, val);
+    v.push_back(temp);
+}
+
+void sort(vector<int> &v)
+{
+    if (v.size() == 1)
+    {
+        return;
+    }
+    int val = v[v.size() - 1];
+    v.pop_back();
+    sort(v);
+    insert(v, val);
+}
+
+int main()
+{
+    vector<int> v;
+    for (int i = 0; i < 5; i++)
+    {
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v);
+
+    for (auto iter : v)
+    {
+        cout << iter << " ";
+    }
+    return 0;
+}
