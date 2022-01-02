a1004b3eb2ae117af14e55e7b729cce26954193e
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-01-02 13:54:05 +0530

    added program

---
+++ recursion/print_subsequence.cpp
@@ -0,0 +1,48 @@
+// Print subsequence whose sum is k
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+int arr[10000001];
+
+void Print_subsequence(int index, int n, vector<int> &ds, int arr[], int sum, int k)
+{
+    if (index == n)
+    {
+        if (sum == k)
+        {
+            for (auto i : ds)
+            {
+                cout << i << " ";
+            }
+            cout << endl;
+        }
+        return;
+    }
+    ds.push_back(arr[index]);
+    sum += arr[index];
+    Print_subsequence(index + 1, n, ds, arr, sum, k);
+    sum -= arr[index];
+    ds.pop_back();
+    Print_subsequence(index + 1, n, ds, arr, sum, k);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, k;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cin >> k;
+    vector<int> ds;
+    Print_subsequence(0, n, ds, arr, 0, k);
+    return 0;
+}
