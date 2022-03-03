e2e2cd49bee7d62d61dd2a60cefb01ba8cee988b
Pradyumn Choudhary <pradyumnchoudhary123@gmail.com>
2022-03-03 11:01:45 +0530

    added string dp based on printing longest subsequences

---
+++ recursion/subsequence_dp_strings.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define tc    \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+
+string lcs(string s, string t)
+{
+    int n = s.size();
+    int m = t.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 0;
+    for (int j = 0; j <= m; j++)
+        dp[0][j] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    int len = dp[n][m];
+
+    string a = "";
+    for (int i = 0; i < len; i++)
+    {
+        a += "$";
+    }
+    int ind = len - 1;
+    int i = n, j = m;
+    while (i > 0 && j > 0)
+    {
+        if (s[i - 1] == t[j - 1])
+        {
+            a[ind] = s[i - 1];
+            i--;
+            j--;
+            ind--;
+        }
+        else if (dp[i - 1][j] > dp[i][j - 1])
+        {
+            i--;
+        }
+        else
+        {
+            j--;
+        }
+    }
+
+    return a;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string a, b;
+    cin >> a >> b;
+    cout << lcs(a, b);
+    return 0;
+}
